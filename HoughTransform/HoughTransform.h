8e00b9832ce609187d3c72b802f9c7f8514b6986
Mikkel <mikkel114@gmail.com>
2020-11-09 10:09:34 +0100

    New hough transform

--- HoughTransform/HoughTransform.h
+++ HoughTransform/HoughTransform.h
@@ -8,24 +8,47 @@
 class HoughTransform
 {
   public:
-    int mainfunc()
-    {
-      //Read image as gray scale
-      cv::Mat  img = cv::imread("circle.jpg", 0);
-      //Blur to reduce noise
-      cv::Mat img_blurred;
-      cv::medianBlur(img, img_blurred, 5);
-      //Vector containing the circles
-      std::vector<cv::Vec3f> circles;
-      //Applying hough transform to the image
-      cv::HoughCircles(img_blurred, circles, cv::HOUGH_GRADIENT, 1, img.rows/64, 200, 10, 5, 30);
-      // Draw detected circles
-      for(size_t i=0; i<circles.size(); i++) {
-          cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-          int radius = cvRound(circles[i][2]);
-          circle(img, center, radius, cv::Scalar(255, 255, 255), 2, 8, 0);
-       }
+    HoughTransform(){};
 
+    cv::Mat mainfunc(cv::Mat img)
+    {
+        // //Read image as gray scale
+        //cv::Mat  img = cv::imread("marble.png", cv::IMREAD_COLOR);
+        //cv::imshow("1", img);
+
+        //Convert to gray and blur
+        cv::Mat gray;
+        cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+        medianBlur(gray, gray, 5);
+
+        //cv::imshow("gray", gray);
+
+        //Vector containing the circles
+        std::vector<cv::Vec3f> circles;
+
+        //Applying hough transform to the image
+        cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1,
+                      gray.rows,  // change this value to detect circles with different distances to each other
+                      100, 20, 0, 0 // change the last two parameters
+                 // (min_radius & max_radius) to detect larger circles, 0 for unknown radius
+         );
+
+        // Draw detected circles
+        for( size_t i = 0; i < circles.size(); i++ )
+        {
+            cv::Vec3i c = circles[i];
+            cv::Point center = cv::Point(c[0], c[1]);
+            // circle center
+            circle( img, center, 1, cv::Scalar(0,100,100), 3, cv::LINE_AA);
+            // circle outline
+            int radius = c[2];
+            circle( img, center, radius, cv::Scalar(255,0,255), 3, cv::LINE_AA);
+        }
+
+        //cv::imshow("final", img);
+        //cv::waitKey();
+
+        return img;
     }
 
 };
